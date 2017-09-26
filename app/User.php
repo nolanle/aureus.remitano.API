a5af0bb011b572e7a4bd9f034980edc8e6de6937
Le The Hoang <hoanglek13@gmail.com>
2017-09-26 10:27:35 +0700

    first config

---
+++ app/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use MoeenBasra\LaravelPassportMongoDB\HasApiTokens;
+
+class User extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use HasApiTokens, Notifiable;
+    use Authenticatable, Authorizable, CanResetPassword;
+
+    protected $collection = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
